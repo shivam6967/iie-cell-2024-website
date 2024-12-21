aeb255a63bfefa9ff6b97a0a5e98e0e44f9d2353
shivammathapati <156904099+shivam6967@users.noreply.github.com>
2024-12-21 16:45:44 +0530

    all the files has been commited

---
+++ iie-cell/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "IIE CELL",
+  description: "Innovation Incubation and Enterpeneurship Cell Wce Sangli",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Favicon */}
+        <link rel="icon" href="" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ iie-cell/data/index.ts
@@ -0,0 +1,248 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Fostering innovation, incubation, and entrepreneurship at Walchand College of Engineering.",
+    description: "Visionary Platform ",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Offering Mentorship, Resources, and Funding",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Guiding innovators from conceptualization",
+    description: "Idea to Impact",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Community Engagement Building networks with Alumni",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: " Facilitating the growth of campus Startups",
+    description: "Startup Launchpad",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "In a world that's becoming more connected every day, the ability to innovate and take risks is more important than ever. The key to building something great isn’t having all the answers upfront, but having the vision to adapt, learn, and grow along the way.",
+    name: "Mark Zuckerberg",
+    title: "Co-founder & CEO, Meta",
+  },
+  {
+    quote:
+      "Success in entrepreneurship comes from resilience and passion. Building something meaningful requires not just talent but the courage to face challenges head-on and turn them into opportunities.",
+    name: "Elon Musk",
+    title: "CEO, Tesla & SpaceX",
+  },
+  {
+    quote:
+      "Here’s to the crazy ones, the misfits, the rebels, the troublemakers—the ones who see things differently. While some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world are the ones who do.",
+    name: "Steve Jobs",
+    title: "Co-founder, Apple",
+  },
+  {
+    quote:
+      "I believe you have to be willing to be misunderstood if you’re going to innovate. We live in a world where customer obsession and a willingness to embrace experimentation are critical. Long-term thinking and continuous innovation create breakthroughs that change the game.",
+    name: "Jeff Bezos",
+    title: "Founder, Amazon",
+  },
+  {
+    quote:
+      "Success is a lousy teacher. It seduces smart people into thinking they can’t lose. The greatest leaders stay grounded, learn from every mistake, and constantly refine their vision. Innovation thrives when you’re not afraid to question what’s possible.",
+    name: "Bill Gates",
+    title: "Co-founder, Microsoft",
+  },
+];
+
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "App Development",
+    desc: "Our team excels in developing innovative mobile applications for both iOS and Android platforms. With a user-centric approach, we create seamless and engaging apps that cater to diverse business needs, enhancing user experience and functionality.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Development",
+    desc: "We specialize in building responsive and high-performance websites. Our expertise spans front-end and back-end technologies, delivering secure, scalable, and user-friendly web applications to meet modern business demands.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Blockchain Technology",
+    desc: "Our blockchain experts provide decentralized solutions that ensure transparency, security, and immutability. We build smart contracts, decentralized apps (DApps), and innovative blockchain-based solutions to empower businesses and industries.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "AI & Machine Learning",
+    desc: "Harness the power of AI and ML with our advanced data-driven solutions. We offer services like predictive modeling, recommendation systems, computer vision, and more, helping businesses make data-backed decisions and automate processes.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 5,
+    title: "Video Editing & Content Creation",
+    desc: "Our creative team brings your brand to life with professional video editing, animation, and content creation. We help businesses engage their audience through high-quality, impactful videos that tell a compelling story.",
+    className: "md:col-span-2",
+    thumbnail: "/exp5.svg",
+  },
+  {
+    id: 6,
+    title: "Marketing Strategy",
+    desc: "We design targeted marketing strategies to boost brand awareness and generate leads. Our services cover digital marketing, SEO, social media management, and performance tracking, helping businesses grow their presence and engage their audience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp6.svg",
+  },
+  {
+    id: 7,
+    title: "Business Development Manager",
+    desc: "Our business development experts work closely with clients to identify opportunities for growth, partnerships, and new market penetration. We offer strategic insights, negotiation skills, and a network of industry contacts to help businesses expand and thrive.",
+    className: "md:col-span-2",
+    thumbnail: "/exp7.svg",
+  },
+  {
+    id: 8,
+    title: "UI/UX Design",
+    desc: "User Interface (UI) and User Experience (UX) design is at the heart of our design philosophy. We create intuitive, aesthetically pleasing designs that focus on providing the best user experience across all platforms.",
+    className: "md:col-span-2",
+    thumbnail: "/exp8.svg",
+  },
+ 
+];
+
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
